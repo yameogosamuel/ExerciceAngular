4ae0431673af9a61f2ff6a5453ca2979a840e09a
yameogosamuel <samuelyameogo2@gmail.com>
2019-11-13 17:32:58 +0100

    first commit

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+export class Dish {
+  id: string;
+  name: string;
+  image: string;
+  category: string;
+  featured: boolean;
+  label: string;
+  price: string;
+  description: string;
+}
