8791a0de15a30de819046e5e0e9cc186ad95bae0
yameogosamuel <samuelyameogo2@gmail.com>
2019-11-25 01:53:26 +0100

    RxJs Part 1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 import {PROMOTIONS} from '../shared/promotions';
+import {Dish} from '../shared/dish';
+import {Observable, of} from 'rxjs';
+import {delay} from 'rxjs/operators';
 
 
 
@@ -12,23 +15,14 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(LEADERS), 2000);
-    });
+  getLeaders():  Observable<Leader[]> {
+    return of(LEADERS).pipe(delay(2000));
   }
-  getLeader(id: string): Promise<Leader> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
-    });
+  getLeader(id: string):  Observable<Leader> {
+    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
   }
 
-  getFeaturedLeader(): Promise<Leader> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
-    });
+  getFeaturedLeader():  Observable<Leader> {
+    return  of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
   }
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
 import {Leader} from '../shared/leader';
+import {Observable, of} from 'rxjs';
+import {delay} from 'rxjs/operators';
+import {LEADERS} from '../shared/leaders';
 
 @Injectable({
   providedIn: 'root'
@@ -11,24 +14,16 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions(): Promise<Promotion[]> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(PROMOTIONS), 2000);
-    });
+  getPromotions():  Observable<Promotion[]> {
+    return of(PROMOTIONS).pipe(delay(2000));
   }
 
-  getPromotion(id: string): Promise<Promotion> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000);
-    });
+  getPromotion(id: string):  Observable<Promotion> {
+    return of(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]).pipe(delay(2000));
   }
 
-  getFeaturedPromotion(): Promise<Promotion> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]), 2000);
-    });
+  getFeaturedPromotion():  Observable<Promotion> {
+    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000));
   }
+
 }
