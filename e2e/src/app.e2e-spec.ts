5a3e36aba33d694589363150b11557f0f3d7ddf8
yameogosamuel <samuelyameogo2@gmail.com>
2019-12-07 02:14:47 +0100

    End-to-End Test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,10 +8,35 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('conFusion app is running!');
+  it('should display message saying Ristorante Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
   });
+
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    const navlink = page.getAllElements('a').get(1);
+    navlink.click();
+
+    expect(page.getParagraphText('h3')).toBe('About Us');
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+
+    const newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+
+    const newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Comment');
+
+    const newSubmitButton = page.getElement('button[type=submit]');
+    newSubmitButton.click();
+
+    browser.pause();
+  });
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,21 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  navigateTo(link: string) {
+    return browser.get(link);
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getParagraphText(selector: string) {
+    return element(by.css(selector)).getText();
   }
+
+  getElement(selector: string) {
+    return element(by.css(selector));
+  }
+
+  getAllElements(selector: string) {
+    return element.all(by.css(selector));
+  }
+
+
 }
